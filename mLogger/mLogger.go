989d70e3dcab7c67d264fefb2592eb2986c173b6
Ishan Goyal <63078216+Ishan27g@users.noreply.github.com>
2021-12-07 18:19:57 +1100

    - output writer and lock

--- mLogger/mLogger.go
+++ mLogger/mLogger.go
@@ -1,13 +1,18 @@
 package mLogger
 
 import (
+	"io"
+	"os"
 	"sync"
 
 	"github.com/hashicorp/go-hclog"
 )
 
 const defaultLevel = "info"
+
 var once sync.Once
+var output io.Writer
+var mx sync.Mutex
 
 // loggers added as Named
 var loggers map[string]hclog.Logger
@@ -17,24 +22,30 @@ var logger hclog.Logger
 
 func init() {
 	once.Do(func() {
-		logger = nil // asserts New is called once
+		mx = sync.Mutex{}
+		logger = nil       // asserts New is called once
+		output = os.Stderr // default out
 		loggers = make(map[string]hclog.Logger)
 	})
 }
 
 // New create a new top level logger with hclog.LevelFromString
-// Subsequent modules should call Get
-func New(name, lvl string) hclog.Logger {
-	m := sync.Mutex{}
-	if lvl == ""{
+// Subsequent modules should call Get.
+// Default level is "info" & default out is stderr
+func New(name, lvl string, out io.Writer) hclog.Logger {
+	if lvl == "" {
 		lvl = defaultLevel
+	}
+	if out != nil {
+		output = out
 	}
 	opts := hclog.LoggerOptions{
 		Name:        "[" + name + "]",
 		Level:       hclog.LevelFromString(lvl),
-		Mutex:       &m,
+		Mutex:       &sync.Mutex{},
 		DisableTime: true,
 		Color:       hclog.AutoColor,
+		Output:      output,
 	}
 	logger = hclog.New(&opts)
 	loggers[name] = hclog.New(&opts)
@@ -45,8 +56,10 @@ func New(name, lvl string) hclog.Logger {
 // returning existing one. If no top level logger exists, the first call to Get
 // creates a top level logger
 func Get(name string) hclog.Logger {
-	if logger == nil{
-		return New(name, defaultLevel)
+	mx.Lock()
+	defer mx.Unlock()
+	if logger == nil {
+		return New(name, defaultLevel, output)
 	}
 	if loggers[name] == nil {
 		loggers[name] = logger.Named(name)
