a0642517a586fea9affc933832206fbbf377be5b
Ishan Goyal <63078216+Ishan27g@users.noreply.github.com>
2022-01-12 10:16:48 +1100

    -update lock

--- fops/fops.go
+++ fops/fops.go
@@ -1,59 +1,60 @@
-package snapshot
+package fops
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-	"time"
-)
-
-func write(filePath string, offset int64, data *[]Entry) int64 {
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0755)
-	if err != nil {
-		return -1
-	}
-	defer func(f *os.File) {
-		_ = f.Close()
-	}(file)
-	var csv string
-	for _, entry := range *data {
-		csv += entry.CreatedAt.Format("2006-01-02T15:04:05.999999999Z07:00") + "," +
-			entry.Data.(string) + "\n"
-	}
-	written, err := file.WriteAt([]byte(csv), offset)
-	if err != nil {
-		fmt.Println("what", err)
-	}
-	return offset + int64(written)
-}
-func read(filePath string) (*[]Entry, int64) {
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0755)
-	if err != nil {
-		return nil, 0
-	}
-	defer func(f *os.File) {
-		_ = f.Close()
-	}(file)
-	var lines []Entry
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		lineWords := strings.Split(scanner.Text(), ",")
-		if len(lineWords) == 0 || len(lineWords) != 2 {
-			fmt.Println("Bad len")
-			continue
-		}
-		time, err := time.Parse(time.RFC3339Nano, lineWords[0])
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		lines = append(lines, Entry{
-			CreatedAt: time,
-			Data:      lineWords[1],
-		})
-	}
-	f, _ := file.Stat()
-	return &lines, f.Size()
-}
+//
+//import (
+//	"bufio"
+//	"fmt"
+//	"os"
+//	"strings"
+//	"time"
+//)
+//
+//func write(filePath string, offset int64, data *[]Entry) int64 {
+//	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0755)
+//	if err != nil {
+//		return -1
+//	}
+//	defer func(f *os.File) {
+//		_ = f.Close()
+//	}(file)
+//	var csv string
+//	for _, entry := range *data {
+//		csv += entry.CreatedAt.Format("2006-01-02T15:04:05.999999999Z07:00") + "," +
+//			entry.Data.(string) + "\n"
+//	}
+//	written, err := file.WriteAt([]byte(csv), offset)
+//	if err != nil {
+//		fmt.Println("what", err)
+//	}
+//	return offset + int64(written)
+//}
+//func read(filePath string) (*[]Entry, int64) {
+//	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0755)
+//	if err != nil {
+//		return nil, 0
+//	}
+//	defer func(f *os.File) {
+//		_ = f.Close()
+//	}(file)
+//	var lines []Entry
+//	scanner := bufio.NewScanner(file)
+//	scanner.Split(bufio.ScanLines)
+//	for scanner.Scan() {
+//		lineWords := strings.Split(scanner.Text(), ",")
+//		if len(lineWords) == 0 || len(lineWords) != 2 {
+//			fmt.Println("Bad len")
+//			continue
+//		}
+//		time, err := time.Parse(time.RFC3339Nano, lineWords[0])
+//		if err != nil {
+//			fmt.Println(err)
+//			continue
+//		}
+//		lines = append(lines, Entry{
+//			CreatedAt: time,
+//			Data:      lineWords[1],
+//		})
+//	}
+//	f, _ := file.Stat()
+//	return &lines, f.Size()
+//}
---
+++ version/version_test.go
@@ -0,0 +1,25 @@
+package version
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var data = []string{"a", "b", "c"}
+
+func TestVersions(t *testing.T) {
+	v := Map()
+	v.UpdateVersion(data...)
+	for _, datum := range data {
+		assert.Equal(t, 1, v.GetVersion(datum))
+	}
+	v.UpdateVersion(data...)
+	for _, datum := range data {
+		assert.Equal(t, 2, v.GetVersion(datum))
+	}
+	v.Delete(data...)
+	for _, datum := range data {
+		assert.Equal(t, -1, v.GetVersion(datum))
+	}
+}
